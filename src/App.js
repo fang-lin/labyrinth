b48bb8c276e61a81fe801252ef210aadadbbadcd
Lin Fang <lin.fang@CNlin.fang>
2019-01-30 17:40:20 +0800

    Add Labyrinth class

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, createElement } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+import Labyrinth from './Labyrinth';
+
+const width = 10;
+
+const Row = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Cell = styled.div`
+  width: ${width}px;
+  height: ${width}px;
+  background-color: #fff;
+  color: #333;
+  font-size: 8px;
+`;
+
+const Road = styled.div`
+  width: ${width}px;
+  height: ${width}px;
+  background-color: #eee;
+  color: #333;
+  font-size: 8px;
+`;
+
+const Wall = styled.div`
+  width: ${width}px;
+  height: ${width}px;
+  background-color: #666;
+  color: #333;
+  font-size: 8px;
+`;
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.labyrinth = new Labyrinth(20);
+    this.state = {
+      labyrinth: this.labyrinth.map,
+    }
+  }
+
+  componentDidMount() {
+    let count = 0;
+    const recordHandler = setInterval(() => {
+      if (this.labyrinth.recorder[count]) {
+        this.setState({
+          labyrinth: this.labyrinth.recorder[count],
+        });
+        count++;
+      } else {
+        clearInterval(recordHandler);
+      }
+    }, 10);
+  }
+
+  createCanvas = (ele) => {
+    this.canvas = ele || null;
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="canvas" ref={ this.createCanvas }>
+          {
+            this.state.labyrinth.map((row, x) => (
+              <Row key={ x }>
+                {
+                  row.map((cell, y) => (
+                    createElement(cell === 0 ? Cell : cell === 2 ? Road : Wall, { key: y })
+                  ))
+                }
+              </Row>
+            ))
+          }
+        </div>
       </div>
     );
   }
---
+++ src/Labyrinth.js
@@ -0,0 +1,97 @@
+import noop from 'lodash/noop';
+import random from 'lodash/random';
+
+export function include(cells, cell) {
+  return cells.get(`${cell[0]},${cell[1]}`);
+}
+
+export function randomCell(cells) {
+  return cells[random(0, cells.length - 1)];
+}
+
+export function initCheckerboard(n, each = noop) {
+  // Zero is road, one is wall
+  const checkerboard = [];
+  const m = n * 2 + 1;
+  for (let x = 0; x < m; x++) {
+    const row = [];
+    for (let y = 0; y < m; y++) {
+      const state = x % 2 === 0 ? 1 : (y % 2 === 0 ? 1 : 0);
+      const returned = each(state, x, y);
+      row.push(typeof returned === 'undefined' ? state : returned);
+    }
+    checkerboard.push(row);
+  }
+  return checkerboard;
+}
+
+export function getAdjoinCell(cell, n) {
+  return [
+    [cell[0] - 2, cell[1]],
+    [cell[0], cell[1] - 2],
+    [cell[0] + 2, cell[1]],
+    [cell[0], cell[1] + 2],
+  ].filter(cell => cell[0] > 0 && cell[0] < n * 2 + 1 && cell[1] > 0 && cell[1] < n * 2 + 1);
+}
+
+export default class Labyrinth {
+  constructor(n) {
+    this.n = n;
+    this.unlinked = new Map();
+    this.recorder = [];
+    this.map = initCheckerboard(n, (state, x, y) => {
+      if (state === 0) {
+        this.unlinked.set(`${x},${y}`, [x, y]);
+      }
+    });
+    this.usableCell = null;
+    this.next([1, 1]);
+    this.recorder = this.recorder.map(map => JSON.parse(map));
+  }
+
+  next(cell) {
+    this.record(this.map);
+
+    if (this.unlinked.size > 0) {
+      let nextCell = this.findRandomUnlinkedAdjoinCell(cell);
+
+      if (nextCell) {
+        const x = (cell[0] + nextCell[0]) / 2;
+        const y = (cell[1] + nextCell[1]) / 2;
+        this.map[x][y] = 2;
+        this.unlinked.delete(`${nextCell[0]},${nextCell[1]}`);
+      } else {
+        nextCell = this.findRandomLinkedAdjoinCell(cell);
+      }
+
+      if (nextCell) {
+        return this.next(nextCell);
+      }
+    }
+  }
+
+  record(map) {
+    const stringify = JSON.stringify(this.map);
+    if (this.recorder[this.recorder.length - 1] !== stringify) {
+      this.recorder.push(stringify);
+    }
+  }
+
+
+  findUnlinkedAdjoinCells(cell) {
+    return getAdjoinCell(cell, this.n).filter(cell => include(this.unlinked, cell));
+  }
+
+  findRandomUnlinkedAdjoinCell(cell) {
+    return randomCell(this.findUnlinkedAdjoinCells(cell));
+  }
+
+  findLinkedAdjoinCells(cell) {
+    return getAdjoinCell(cell, this.n).filter(cell => !include(this.unlinked, cell));
+  }
+
+  findRandomLinkedAdjoinCell(cell) {
+    return randomCell(this.findLinkedAdjoinCells(cell));
+  }
+
+}
